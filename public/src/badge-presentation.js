afbc2c94faa0410351a4deee4f20a913e24a3fe5
John Gorter <john.gorter@infosupport.com>
2018-04-15 14:21:29 +0200

    final

--- public/src/badge-presentation.js
+++ public/src/badge-presentation.js
@@ -117,8 +117,8 @@ export class BadgePresentation extends GestureEventListeners(PolymerElement) {
     }
     
     _sort(a, b) {
-        var y =  new Date(a.ApplicationDate);
-        var x = new Date(b.ApplicationDate);
+        var y =  new Date(a.LastName);
+        var x = new Date(b.LastName);
         return x < y ? -1 : x > y ? 1 : 0;
     }
 
